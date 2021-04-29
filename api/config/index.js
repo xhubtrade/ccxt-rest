c23f88ea7e34f3124891ee44501c86d05d4d59cd
Daniel S <admin@developeroncall.com>
2021-04-29 13:16:46 -0400

    Update index.js

--- api/config/index.js
+++ api/config/index.js
@@ -2,6 +2,6 @@ module.exports = {
     dbConfigPath : process.env.DB_CONFIG,
     env : process.env.NODE_ENV,
     cors : process.env.CORS || '*',
-    logLevel : (process.env.LOG_LEVEL || 'error'),
+    logLevel : (process.env.LOG_LEVEL || 'emerg'),
     port : (process.env.PORT || 3000)
 }
